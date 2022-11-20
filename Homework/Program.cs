d6e4cffa77768e8e04975982d80c117f32ea3700
Ramid Salmanov <ramidsalmanov@yandex.ru>
2022-11-20 16:35:37 +0400

    feebback

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,11 +1,14 @@
 ﻿Console.WriteLine("Вас приветсвует УГАДАЙКА! Попробуйте угадать число (число положительное, от 1 до 500), которое загадала программа.");
 int secret = new Random().Next(1, 500);
-int UserNumber;
+int UserNumber; // названия переменных пишем в camelCase, загугли
+
+// кривое форматированмие, нажми Ctrl + K + D
     while (!int.TryParse(Console.ReadLine(), out UserNumber) || UserNumber <= 0 || UserNumber > 500 || UserNumber != secret)
 {
     Console.WriteLine("Данные введены некоректно или введенное Вами число не совпадает с числом программы! Попробуйте снова");
 }
 
-Console.WriteLine( "Поздравляем! Вы угадали число" + UserNumber);   
-
-
+Console.WriteLine( "Поздравляем! Вы угадали число" + UserNumber);
+// лишняя пустая строка
+// лишняя пустая строка
+// лишняя пустая строка
