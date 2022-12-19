728225ef1290437aa78144003f0aae76c25a5202
ArtemChichaev <chichaevartem@gmail.com>
2022-12-19 13:27:44 +0400

    final

---
+++ WAS/Goods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAS
+{
+    internal class Goods
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+
+        public Goods(int id, string name, string description, string category, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Category = category;
+            Quantity = quantity;
+        }
+    }
+
+}
---
+++ WAS/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace WAS
+{
+    internal class Program
+    {
+        static Dictionary<int, Goods> GoodsDict = new();
+        static Dictionary<int, string> ActionsWithGoodsDict = new()
+        {
+            {1,"Поиск товара"},
+            {2,"Вывод названия и количества товара"},
+            {3,"Добавить"},
+            {4,"Удалить"},
+        };
+        static void Main(string[] args)
+        {
+            PrintWelcome();
+            foreach ((int key, string value) in ActionsWithGoodsDict)
+            {
+                Console.WriteLine(key + " " + value);
+            }
+            var actionKey = ActionGoods("Введите номер действия", ActionsWithGoodsDict.Keys.First(), ActionsWithGoodsDict.Keys.Last());
+            switch (ActionsWithGoodsDict[actionKey])
+            {
+                case "Добавить":
+                    var good = GetNewGood();
+                    AddGood(good);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private static void PrintWelcome()
+        {
+            Console.WriteLine("Вас приветствует система складского учета WAS!");
+            Console.WriteLine();
+        }
+        private static int ActionGoods(string message, int startNumber, int endNumber)
+        {
+            Console.WriteLine(message);
+            int number = 0;
+            while (!int.TryParse(Console.ReadLine(), out number) || number < startNumber || number > endNumber)
+            {
+                Console.WriteLine("Данные введены некорректно. Попробуйте снова.");
+            }
+            return number;
+        }
+        private static void AddGood(Goods good)
+        {
+            if (good == null)
+            {
+                return;
+            }
+            if (GoodsDict.Contains(good.Id))
+            {
+                Console.WriteLine("Товар с таким Id уже существует.");
+                return;
+            }
+            GoodsDict.Add(good.Id, good);
+        }
+        private static Goods GetNewGood()
+        {
+            Console.WriteLine("Введите наименование, описание и категорию товара.");
+            var name = Console.ReadLine();
+            var description = Console.ReadLine();
+            var category = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            var gds = new Goods(0, name, description, category, quantity);
+            return gds;
+        }
+        private static string GetStringFromUser()
+        {
+            var value = Console.ReadLine();
+            while (string.IsNullOrEmpty(value))
+            {
+                Console.WriteLine("Пустого значения быть не может.");
+                value = Console.ReadLine();
+            }
+            return value;
+        }
+    }
+}
